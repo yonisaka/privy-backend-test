483495d95bbce5c5ed5cbefdb8e5c3f704904a4c
yonisaka <yonisaka0@gmail.com>
2022-11-29 22:37:56 +0700

    add parse time connection

--- database/20221128135645_seeder_cake.sql
+++ database/20221128135645_seeder_cake.sql
@@ -2,11 +2,11 @@
 -- +goose StatementBegin
 INSERT INTO cakes (`id`, `title`, `description`, `rating`, `image`, `created_at`, `updated_at`)
 VALUES 
-    (1, "Lemon cheesecake", "A cheesecake made of lemon", 7, NULL, NOW(), NOW()),
-    (2, "Cake cake", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam", 5.5, NULL, NOW(), NOW());
+    (1, "Lemon cheesecake", "A cheesecake made of lemon", 7, "no-image.jpeg", NOW(), NOW()),
+    (2, "Cake cake", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam", 5.5, "no-image.jpeg", NOW(), NOW());
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-SELECT 'down SQL query';
+DELETE FROM cakes;
 -- +goose StatementEnd
